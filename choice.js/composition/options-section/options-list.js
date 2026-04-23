5574c4f3d3a17eb48c354ed34935f498421784fc
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-04-23 18:18:15 +0100

    options web component

---
+++ choice.js/composition/options-section/options-list.js
@@ -0,0 +1,265 @@
+import { OptionListManager } from "./OptionListManager.js";
+class OptionsList extends HTMLElement {
+  _manager = null;
+  _options = {};
+  _attributeEvents = {};
+  static get observedAttributes() {
+    return [
+      "options",
+      "loading",
+      "value",
+      "label",
+      "disabled",
+      "max-height",
+      "show-footer",
+      "show-filter",
+      "onItemClick",
+      "onInputChange",
+      "onCancel",
+      "onOk",
+      "render-empty",
+      "render-item",
+      "render-list"
+    ];
+  }
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    if (!this._manager) {
+      this._initManager();
+    }
+  }
+  _initManager() {
+    this._options = {
+      options: (() => {
+        try {
+          return JSON.parse(this.getAttribute("options") || "[]");
+        } catch (e) {
+          return [];
+        }
+      })(),
+      loading: this.hasAttribute("loading"),
+      value: this.getAttribute("value") || "",
+      label: this.getAttribute("label") || "",
+      disabled: this.hasAttribute("disabled"),
+      maxHeight: this.getAttribute("max-height") || "",
+      showFooter: this.hasAttribute("show-footer") ? this.getAttribute("show-footer") !== "false" : true,
+      showFilter: this.hasAttribute("show-filter") ? this.getAttribute("show-filter") !== "false" : true,
+      onItemClick: (item) => {
+        this.dispatchEvent(new CustomEvent("onItemClick", { detail: { item } }));
+      },
+      onInputChange: (e) => {
+        this.dispatchEvent(
+          new CustomEvent("onInputChange", {
+            detail: { originalEvent: e, value: e.target.value }
+          })
+        );
+      },
+      onCancel: () => {
+        this.dispatchEvent(new CustomEvent("onCancel"));
+      },
+      onOk: () => {
+        this.dispatchEvent(new CustomEvent("onOk"));
+      }
+    };
+    const renderEmpty = this.getAttribute("render-empty");
+    if (renderEmpty) {
+      this._options.renderEmpty = () => {
+        return new Function("manager", renderEmpty).call(this, this._manager);
+      };
+    }
+    const renderItem = this.getAttribute("render-item");
+    if (renderItem) {
+      this._options.renderItem = (item) => {
+        return new Function("item", "manager", renderItem).call(this, item, this._manager);
+      };
+    }
+    const renderList = this.getAttribute("render-list");
+    if (renderList) {
+      this._options.renderList = (list) => {
+        return new Function("list", "manager", renderList).call(this, list, this._manager);
+      };
+    }
+    ["onItemClick", "onInputChange", "onCancel", "onOk"].forEach((attr) => {
+      const val = this.getAttribute(attr);
+      if (val) this._setupAttributeEvent(attr, val);
+    });
+    this._manager = new OptionListManager(this, this._options);
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (!this._manager) return;
+    switch (name) {
+      case "options":
+        try {
+          this._manager.setOptions(JSON.parse(newValue));
+        } catch (e) {
+          console.error("Invalid JSON in options attribute", newValue);
+        }
+        break;
+      case "loading":
+        this._manager.setLoading(this.hasAttribute("loading"));
+        break;
+      case "value":
+        this._manager.setValue(newValue);
+        break;
+      case "label":
+        this._manager.setLabel(newValue);
+        break;
+      case "disabled":
+        this._manager.setDisabled(this.hasAttribute("disabled"));
+        break;
+      case "max-height":
+        this._manager.setMaxHeight(newValue);
+        break;
+      case "show-footer":
+        this._manager.showFooter(newValue !== "false");
+        break;
+      case "show-filter":
+        this._manager.showFilter(newValue !== "false");
+        break;
+      case "render-empty":
+        if (newValue) {
+          this._manager.propOptions.renderEmpty = () => {
+            return new Function("manager", newValue).call(this, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "render-item":
+        if (newValue) {
+          this._manager.propOptions.renderItem = (item) => {
+            return new Function("item", "manager", newValue).call(this, item, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "render-list":
+        if (newValue) {
+          this._manager.propOptions.renderList = (list) => {
+            return new Function("list", "manager", newValue).call(this, list, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "onItemClick":
+      case "onInputChange":
+      case "onCancel":
+      case "onOk":
+        this._setupAttributeEvent(name, newValue);
+        break;
+    }
+  }
+  _setupAttributeEvent(name, value) {
+    if (this._attributeEvents[name]) {
+      this.removeEventListener(name, this._attributeEvents[name]);
+      delete this._attributeEvents[name];
+    }
+    if (value) {
+      this._attributeEvents[name] = (e) => {
+        new Function("event", value).call(this, e);
+      };
+      this.addEventListener(name, this._attributeEvents[name]);
+    }
+  }
+  // Proxied methods
+  setMaxHeight(maxHeight) {
+    if (maxHeight) this.setAttribute("max-height", maxHeight);
+    else this.removeAttribute("max-height");
+    this._manager?.setMaxHeight(maxHeight);
+  }
+  setDisabled(disabled) {
+    if (disabled) this.setAttribute("disabled", "");
+    else this.removeAttribute("disabled");
+    this._manager?.setDisabled(disabled);
+  }
+  showFooter(show) {
+    this.setAttribute("show-footer", String(show));
+    this._manager?.showFooter(show);
+  }
+  showFilter(show) {
+    this.setAttribute("show-filter", String(show));
+    this._manager?.showFilter(show);
+  }
+  setOptions(options) {
+    // we don't necessarily want to stringify options back to attribute for performance and avoiding circularity
+    // but we can if we want to stay in sync. SelectedList doesn't seem to do it for 'list'.
+    // this.setAttribute("options", JSON.stringify(options)); 
+    this._manager?.setOptions(options);
+  }
+  updateOptions(options) {
+    this.setOptions(options);
+  }
+  setLoading(loading) {
+    if (loading) this.setAttribute("loading", "");
+    else this.removeAttribute("loading");
+    this._manager?.setLoading(loading);
+  }
+  setValue(value) {
+    this.setAttribute("value", value);
+    this._manager?.setValue(value);
+  }
+  setLabel(label) {
+    this.setAttribute("label", label);
+    this._manager?.setLabel(label);
+  }
+  setFocus() {
+    this._manager?.setFocus();
+  }
+  render() {
+    this._manager?.render();
+  }
+  // Getters and setters
+  get options() {
+    return this._manager?.propOptions.options || [];
+  }
+  set options(val) {
+    this.setOptions(val);
+  }
+  get loading() {
+    return this.hasAttribute("loading");
+  }
+  set loading(val) {
+    this.setLoading(val);
+  }
+  get value() {
+    return this._manager?.propInputElement?.value ?? this.getAttribute("value") ?? "";
+  }
+  set value(val) {
+    this.setValue(val);
+  }
+  get label() {
+    return this.getAttribute("label") || "";
+  }
+  set label(val) {
+    this.setLabel(val);
+  }
+  get disabled() {
+    return this.hasAttribute("disabled");
+  }
+  set disabled(val) {
+    this.setDisabled(val);
+  }
+  get maxHeight() {
+    return this.getAttribute("max-height") || "";
+  }
+  set maxHeight(val) {
+    this.setMaxHeight(val);
+  }
+  get footer() {
+    return this.hasAttribute("show-footer") ? this.getAttribute("show-footer") !== "false" : true;
+  }
+  set footer(val) {
+    this.showFooter(val);
+  }
+  get filter() {
+    return this.hasAttribute("show-filter") ? this.getAttribute("show-filter") !== "false" : true;
+  }
+  set filter(val) {
+    this.showFilter(val);
+  }
+}
+customElements.define("options-list", OptionsList);
+export {
+  OptionsList
+};
---
+++ choice.js/composition/options-section/options-list.ts
@@ -0,0 +1,304 @@
+import { OptionListManager, ListElement, OptionListManagerOptions } from "./OptionListManager.js";
+
+export class OptionsList extends HTMLElement {
+  private _manager: OptionListManager<ListElement> | null = null;
+  private _options: OptionListManagerOptions<ListElement> = {};
+  private _attributeEvents: Record<string, any> = {};
+
+  static get observedAttributes() {
+    return [
+      "options",
+      "loading",
+      "value",
+      "label",
+      "disabled",
+      "max-height",
+      "show-footer",
+      "show-filter",
+      "onItemClick",
+      "onInputChange",
+      "onCancel",
+      "onOk",
+      "render-empty",
+      "render-item",
+      "render-list",
+    ];
+  }
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    if (!this._manager) {
+      this._initManager();
+    }
+  }
+
+  private _initManager() {
+    this._options = {
+      options: (() => {
+        try {
+          return JSON.parse(this.getAttribute("options") || "[]");
+        } catch (e) {
+          return [];
+        }
+      })(),
+      loading: this.hasAttribute("loading"),
+      value: this.getAttribute("value") || "",
+      label: this.getAttribute("label") || "",
+      disabled: this.hasAttribute("disabled"),
+      maxHeight: this.getAttribute("max-height") || "",
+      showFooter: this.hasAttribute("show-footer") ? this.getAttribute("show-footer") !== "false" : true,
+      showFilter: this.hasAttribute("show-filter") ? this.getAttribute("show-filter") !== "false" : true,
+      onItemClick: (item) => {
+        this.dispatchEvent(new CustomEvent("onItemClick", { detail: { item } }));
+      },
+      onInputChange: (e) => {
+        this.dispatchEvent(
+          new CustomEvent("onInputChange", {
+            detail: { originalEvent: e, value: (e.target as HTMLInputElement).value },
+          }),
+        );
+      },
+      onCancel: () => {
+        this.dispatchEvent(new CustomEvent("onCancel"));
+      },
+      onOk: () => {
+        this.dispatchEvent(new CustomEvent("onOk"));
+      },
+    };
+
+    const renderEmpty = this.getAttribute("render-empty");
+    if (renderEmpty) {
+      this._options.renderEmpty = () => {
+        return new Function("manager", renderEmpty).call(this, this._manager);
+      };
+    }
+
+    const renderItem = this.getAttribute("render-item");
+    if (renderItem) {
+      this._options.renderItem = (item) => {
+        return new Function("item", "manager", renderItem).call(this, item, this._manager);
+      };
+    }
+
+    const renderList = this.getAttribute("render-list");
+    if (renderList) {
+      this._options.renderList = (list) => {
+        return new Function("list", "manager", renderList).call(this, list, this._manager);
+      };
+    }
+
+    ["onItemClick", "onInputChange", "onCancel", "onOk"].forEach((attr) => {
+      const val = this.getAttribute(attr);
+      if (val) this._setupAttributeEvent(attr, val);
+    });
+
+    this._manager = new OptionListManager(this, this._options);
+  }
+
+  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
+    if (!this._manager) return;
+
+    switch (name) {
+      case "options":
+        try {
+          this._manager.setOptions(JSON.parse(newValue));
+        } catch (e) {
+          console.error("Invalid JSON in options attribute", newValue);
+        }
+        break;
+      case "loading":
+        this._manager.setLoading(this.hasAttribute("loading"));
+        break;
+      case "value":
+        this._manager.setValue(newValue);
+        break;
+      case "label":
+        this._manager.setLabel(newValue);
+        break;
+      case "disabled":
+        this._manager.setDisabled(this.hasAttribute("disabled"));
+        break;
+      case "max-height":
+        this._manager.setMaxHeight(newValue);
+        break;
+      case "show-footer":
+        this._manager.showFooter(newValue !== "false");
+        break;
+      case "show-filter":
+        this._manager.showFilter(newValue !== "false");
+        break;
+      case "render-empty":
+        if (newValue) {
+          this._manager.propOptions.renderEmpty = () => {
+            return new Function("manager", newValue).call(this, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "render-item":
+        if (newValue) {
+          this._manager.propOptions.renderItem = (item) => {
+            return new Function("item", "manager", newValue).call(this, item, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "render-list":
+        if (newValue) {
+          this._manager.propOptions.renderList = (list) => {
+            return new Function("list", "manager", newValue).call(this, list, this._manager);
+          };
+        }
+        this._manager.render();
+        break;
+      case "onItemClick":
+      case "onInputChange":
+      case "onCancel":
+      case "onOk":
+        this._setupAttributeEvent(name, newValue);
+        break;
+    }
+  }
+
+  private _setupAttributeEvent(name: string, value: string) {
+    if (this._attributeEvents[name]) {
+      this.removeEventListener(name, this._attributeEvents[name]);
+      delete this._attributeEvents[name];
+    }
+
+    if (value) {
+      this._attributeEvents[name] = (e: any) => {
+        new Function("event", value).call(this, e);
+      };
+      this.addEventListener(name, this._attributeEvents[name]);
+    }
+  }
+
+  // Proxied methods
+  public setMaxHeight(maxHeight?: string) {
+    if (maxHeight) this.setAttribute("max-height", maxHeight);
+    else this.removeAttribute("max-height");
+    this._manager?.setMaxHeight(maxHeight);
+  }
+
+  public setDisabled(disabled: boolean) {
+    if (disabled) this.setAttribute("disabled", "");
+    else this.removeAttribute("disabled");
+    this._manager?.setDisabled(disabled);
+  }
+
+  public showFooter(show: boolean) {
+    this.setAttribute("show-footer", String(show));
+    this._manager?.showFooter(show);
+  }
+
+  public showFilter(show: boolean) {
+    this.setAttribute("show-filter", String(show));
+    this._manager?.showFilter(show);
+  }
+
+  public setOptions(options: ListElement[]) {
+    // we don't necessarily want to stringify options back to attribute for performance and avoiding circularity
+    // but we can if we want to stay in sync. SelectedList doesn't seem to do it for 'list'.
+    // this.setAttribute("options", JSON.stringify(options)); 
+    this._manager?.setOptions(options);
+  }
+
+  public updateOptions(options: ListElement[]) {
+    this.setOptions(options);
+  }
+
+  public setLoading(loading: boolean) {
+    if (loading) this.setAttribute("loading", "");
+    else this.removeAttribute("loading");
+    this._manager?.setLoading(loading);
+  }
+
+  public setValue(value: string) {
+    this.setAttribute("value", value);
+    this._manager?.setValue(value);
+  }
+
+  public setLabel(label: string) {
+    this.setAttribute("label", label);
+    this._manager?.setLabel(label);
+  }
+
+  public setFocus() {
+    this._manager?.setFocus();
+  }
+
+  public render() {
+    this._manager?.render();
+  }
+
+  // Getters and setters
+  get options() {
+    return this._manager?.propOptions.options || [];
+  }
+
+  set options(val: ListElement[]) {
+    this.setOptions(val);
+  }
+
+  get loading() {
+    return this.hasAttribute("loading");
+  }
+
+  set loading(val: boolean) {
+    this.setLoading(val);
+  }
+
+  get value() {
+    return this._manager?.propInputElement?.value ?? this.getAttribute("value") ?? "";
+  }
+
+  set value(val: string) {
+    this.setValue(val);
+  }
+
+  get label() {
+    return this.getAttribute("label") || "";
+  }
+
+  set label(val: string) {
+    this.setLabel(val);
+  }
+
+  get disabled() {
+    return this.hasAttribute("disabled");
+  }
+
+  set disabled(val: boolean) {
+    this.setDisabled(val);
+  }
+
+  get maxHeight() {
+    return this.getAttribute("max-height") || "";
+  }
+
+  set maxHeight(val: string) {
+    this.setMaxHeight(val);
+  }
+
+  get footer() {
+    return this.hasAttribute("show-footer") ? this.getAttribute("show-footer") !== "false" : true;
+  }
+
+  set footer(val: boolean) {
+    this.showFooter(val);
+  }
+
+  get filter() {
+    return this.hasAttribute("show-filter") ? this.getAttribute("show-filter") !== "false" : true;
+  }
+
+  set filter(val: boolean) {
+    this.showFilter(val);
+  }
+}
+
+customElements.define("options-list", OptionsList);
