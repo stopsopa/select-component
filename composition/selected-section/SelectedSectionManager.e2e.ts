a56671a6a80b19380ed1be7b616a8ee331981458
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-21 02:11:09 +0100

    g

--- composition/selected-section/SelectedSectionManager.e2e.ts
+++ composition/selected-section/SelectedSectionManager.e2e.ts
@@ -7,17 +7,92 @@ import { softNavigate, querySelector, clickSelector, prepare, compareSelectedIte
  * /bin/bash playwright.sh -- composition/selected-section/SelectedSectionManager.e2e.ts
  * /bin/bash playwright.sh -- --debug -- composition/selected-section/SelectedSectionManager.e2e.ts
  *
- * ./node_modules/.bin/playwright codegen http://0.0.0.0:5678/vite-project/dist/
+ * ./node_modules/.bin/playwright codegen http://0.0.0.0:5678/composition/selected-section/SelectedSectionManager.html
  *
  * /bin/bash playwright.sh -- composition/selected-section/SelectedSectionManager.e2e.ts -g "build list"
  * /bin/bash playwright.sh -- --debug -g "build list" -- composition/selected-section/SelectedSectionManager.e2e.ts
  *
  */
-test("nvaite to preselect 3", async ({ page }) => {
+test("clear default selection", async ({ page }) => {
   await page.goto("/composition/selected-section/SelectedSectionManager.html");
   await page.getByPlaceholder(" ").click();
   await page.locator(".flex-list > div:nth-child(2) > div").click();
   await page.locator(".element > div").click();
 
   await compareSelectedItems(page, '[data-role="dump"]', []);
+});
+test("add 2 manual and one clicked", async ({ page }) => {
+  await page.goto("/composition/selected-section/SelectedSectionManager.html");
+  await page.getByPlaceholder(" ").click();
+  await page.getByPlaceholder(" ").fill("abc");
+  await page.getByPlaceholder(" ").press("Enter");
+  await page.getByPlaceholder(" ").fill("test");
+  await page.getByPlaceholder(" ").press("Enter");
+  await page.getByRole("button", { name: "t3chat.png" }).click();
+
+  await compareSelectedItems(page, '[data-role="dump"]', [
+    {
+      id: 1,
+      label: "Initial 1",
+      selected: true,
+    },
+    {
+      id: 2,
+      label: "Initial 2",
+      selected: true,
+    },
+    {
+      id: 223,
+      label: "abc",
+      selected: true,
+    },
+    {
+      id: 224,
+      label: "test",
+      selected: true,
+    },
+    {
+      color: "green",
+      id: 225,
+      img: "t3chat.png",
+      label: "t3chat",
+      selected: true,
+    },
+  ]);
+});
+
+/**
+ * /bin/bash playwright.sh -- composition/selected-section/SelectedSectionManager.e2e.ts -g "check disabled"
+ */
+test("check disabled", async ({ page }) => {
+  await page.goto("/composition/selected-section/SelectedSectionManager.html");
+
+  const element = await querySelector(page, ".selected-section");
+
+  // on start it shoulnd't have disabled class
+  await expect(element).not.toHaveClass(/disabled/);
+
+  await page.getByRole("checkbox", { name: "Disabled" }).check();
+
+  // and after check it should have disabled class
+  await expect(element).toHaveClass(/disabled/);
+
+  const url = await page.evaluate(() => {
+    return window.location.pathname + window.location.search;
+  });
+
+  expect(url).toBe(
+    "/composition/selected-section/SelectedSectionManager.html?v1=Initial+1&v1=Initial+2&l1=50px&c1=350px&d1=1&o1=0&as1=Select+options&s1=&e1=0&i1=1",
+  );
+
+  await compareSelectedItems(page, '[data-role="dump"]', [
+    {
+      id: 1,
+      label: "Initial 1",
+    },
+    {
+      id: 2,
+      label: "Initial 2",
+    },
+  ]);
 });
