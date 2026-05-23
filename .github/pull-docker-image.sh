7e7aaf6ee5da4a449956e2d2c86faaf9ab3ecde3
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-24 00:00:51 +0100

    fix: pull image

--- .github/pull-docker-image.sh
+++ .github/pull-docker-image.sh
@@ -6,3 +6,10 @@ set -o pipefail
 IMAGE="$(/bin/bash playwright.sh --image)"
 
 docker pull "${IMAGE}"
+
+SIZE="$(docker image inspect "${IMAGE}" --format='{{.Size}}')"
+
+# human readable
+SIZE_HUMAN="$(numfmt --to=iec-i --suffix=B "${SIZE}")"
+
+echo "Pulled image size: ${SIZE_HUMAN}"
