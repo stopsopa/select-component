58863165d4f4370e03675dd92cd8fc01f708e7a7
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-24 01:50:32 +0100

    d

--- .github/fix-cjs.sh
+++ .github/fix-cjs.sh
@@ -1,8 +1,9 @@
 set -e
 echo "${0}: fixing require(*.js) to require(*.cjs)"
-for file in ./dist/cjs/*.js; do
+find ./dist/cjs -type f -name "*.js" | while read -r file; do
   echo "Renaming ${file} to ${file%.js}.cjs..."
   perl -pi -e 's#require\("(.*?)\.js"\)#require\("\1.cjs"\)#g' "${file}"
   mv "${file}" "${file%.js}.cjs"
 done
+
 
--- .github/fix-mjs.sh
+++ .github/fix-mjs.sh
@@ -1,8 +1,9 @@
 set -e
 echo "${0}: fixing import(*.js) to import(*.mjs)"
-for file in ./dist/esm/*.js; do
+find ./dist/esm -type f -name "*.js" | while read -r file; do
   echo "Renaming ${file} to ${file%.js}.mjs..."
   perl -pi -e 's#import ([^\s]+) from "(.*?)\.js";#import \1 from "\2.mjs";#g' "${file}"
   mv "${file}" "${file%.js}.mjs"
 done
+
 
